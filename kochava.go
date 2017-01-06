86966063b33a504a543da2caf96e377ccdab879e
Corbin Staaben <cstaaben@gmail.com>
2017-01-06 01:23:55 -0800

    Updated .gitignore to include Windows executables, Redis file backups, and IntelliJ project files. kochava.go now successfully connects to the Redis server at the specified port, with successful authentication.

--- kochava.go
+++ kochava.go
@@ -1,10 +1,28 @@
 package main
 
 import (
-	"github.com/mediocregopher/radix.v2/pool"
+	redis "gopkg.in/redis.v5"
 	"fmt"
 )
 
+// Create and return a *redis.Client connected to the default
+func redisConnect(addr string, password string, db int) (*redis.Client, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr: addr,
+		Password: password,
+		DB: db,
+	})
+
+	_, err := client.Ping().Result()
+
+	return client, err
+}
+
 func main() {
-	
+	_, err := redisConnect("127.0.0.1:7331", "staaben-miniProj", 0)
+	if(err != nil) {
+		fmt.Println(err)
+	}
+
+
 }
